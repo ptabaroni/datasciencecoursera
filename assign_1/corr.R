67cba95a104e6d579ce19c51154c772738eaed11
ptabaroni <tabaroni+github@gmail.com>
2014-11-13 19:45:26 +0100

    new file:   assign_1/corr.R 	modified:   Missing-test 7

---
+++ assign_1/corr.R
@@ -0,0 +1,20 @@
+corr <- function(directory, threshold = 0) {
+  valid <- complete(directory)
+  mask<-subset(valid,nobs>threshold)
+  selection<- mask$id
+  ans<-numeric()
+  if (length(selection)>0)
+  {
+  monitor<-file.path(directory,paste(formatC(selection,width=3, flag="0"),".csv",sep=""), fsep = "/" )
+  
+  for (i in 1:length(monitor))
+  {
+    ds <- read.csv(monitor[i], header =TRUE, sep=",")
+    a<-ds[["sulfate"]]
+    b<-ds[["nitrate"]]
+    ans <-append(ans, cor(a,b,use="complete.obs"))
+  }
+}
+ round(ans,5)
+
+}
