8dcc67a28a440556908d84ce2b0593bcfe982a0d
ptabaroni <tabaroni+github@gmail.com>
2014-11-26 17:56:32 +0100

    Removed Extra lines

--- assign_1/complete.R
+++ assign_1/complete.R
@@ -1,19 +1,13 @@
 complete <- function(directory, ids = 1:332) {
-  monitor<-file.path(directory,paste(formatC(ids,width=3, flag="0"),".csv",sep=""), fsep = "/" )
-  id <-NULL
-  nobs <-NULL
-  for (i in 1:length(monitor))
-   {
-    ds <- read.csv(monitor[i], header =TRUE, sep=",")
-    eval <- nrow(ds[complete.cases(ds),])
-    #if (eval > 0)
-    #{
-      id<-append(id,ids[[i]])
-      nobs<-append(nobs,eval)
-    #}
-  }
-#final<-data.frame(id,nobs)
-#colnames(final)<-(c("id","nobs"))
-#return(final)
-return(data.frame(id, nobs))
+        monitor<-file.path(directory,paste(formatC(ids,width=3, flag="0"),".csv",sep=""), fsep = "/" )
+        id <-NULL
+        nobs <-NULL
+        for (i in 1:length(monitor))
+        {
+                ds <- read.csv(monitor[i], header =TRUE, sep=",")
+                eval <- nrow(ds[complete.cases(ds),])
+                id<-append(id,ids[[i]])
+                nobs<-append(nobs,eval)
+        }
+        return(data.frame(id, nobs))        
 }
